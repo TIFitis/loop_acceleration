ab4e91eb21299fb32170e8020679b8f361e6006c
Akash Banerjee <TIFitis@github.com>
2019-10-26 15:05:25 +0530

    muted commands in make

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -22,8 +22,7 @@ CPP_DEPS += \
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I$(CBMC_DIR)"/src" -O0 -g3 -Wall -c -fmessage-length=0 -Wno-deprecated -Wno-deprecated-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@g++ -I$(CBMC_DIR)"/src" -O0 -g3 -Wall -c -fmessage-length=0 -Wno-deprecated -Wno-deprecated-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
